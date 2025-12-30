3205a893f90a5b30f4ac1eec45aff26fdce92c78
shinyleefeon <leecastro7@gmail.com>
2025-12-30 08:36:19 -0500

    Basic server with a handler that checks for specific paths. First commit from vscode

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/shinyleefeon/Chirpy.git
+
+go 1.25.4
---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"net/http"
+	"fmt"
+	"sync/atomic"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (c *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		c.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func main() {
+	const filepathRoot = "."
+	apiCFG := &apiConfig{}
+	mux := http.NewServeMux()
+	mux.Handle("/app/", apiCFG.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
+	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	mux.HandleFunc("GET /admin/metrics", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(fmt.Sprintf(`<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>`, apiCFG.fileserverHits.Load())))
+	})
+
+	mux.HandleFunc("POST /admin/reset", func(w http.ResponseWriter, r *http.Request) {
+		apiCFG.fileserverHits.Store(0)
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Metrics reset\n"))
+	})
+	
+	Server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+	fmt.Printf("Starting server at dir %s\n", filepathRoot)
+	Server.ListenAndServe()
+}
