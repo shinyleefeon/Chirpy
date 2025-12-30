74195d4557518fce69f2c688897c9b7af339f945
shinyleefeon <leecastro7@gmail.com>
2025-12-30 10:40:43 -0500

    "Added sql db and sqlc queries"

--- main.go
+++ main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
-	"net/http"
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
 	"sync/atomic"
+	"strings"
+	"database/sql"
+	"os"
+	"github.com/shinyleefeon/Chirpy.git/internal/database"
+	"github.com/joho/godotenv"
 )
+
+import _ "github.com/lib/pq"
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	database	  *database.Queries
 }
 
 func (c *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
@@ -17,15 +27,87 @@ func (c *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	})
 }
 
+func censorBody(body string) string {
+	badWords := []string{"kerfuffle", "sharbert", "fornax"}
+	censored := strings.Split(body, " ")
+	for _, badWord := range badWords {
+		for i, word := range censored {
+			if strings.ToLower(word) == badWord {
+				censored[i] = "****"
+			}
+		}
+	}
+	result := strings.Join(censored, " ")
+	return result
+}
+
+func respondWithError(w http.ResponseWriter, statusCode int, message string) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(statusCode)
+	w.Write([]byte(message + "\n"))
+}
+
+func respondWithJSON(w http.ResponseWriter, statusCode int, payload interface{}) {
+	data, err := json.Marshal(payload)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error marshalling JSON")
+		w.WriteHeader(500)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(statusCode)
+	w.Write(data)
+}
+
 func main() {
-	const filepathRoot = "."
 	apiCFG := &apiConfig{}
+	godotenv.Load()
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Error connecting to database: %s", err)
+	}
+	defer db.Close()
+	dbQueries := database.New(db)
+	apiCFG.database = dbQueries
+	const filepathRoot = "."
+	
 	mux := http.NewServeMux()
 	mux.Handle("/app/", apiCFG.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))))
 	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
+	})
+	mux.HandleFunc("POST /api/validate_chirp", func(w http.ResponseWriter, r *http.Request) {
+		type paramaters struct {
+			Body string `json:"body"`
+		}
+		params := paramaters{}
+		decoder := json.NewDecoder(r.Body)
+		err := decoder.Decode(&params)
+		if err != nil {
+			log.Printf("Error decoding parameters: %s", err)
+			w.WriteHeader(500)
+			return
+		}
+		if len(params.Body) == 0 || len(params.Body) > 140 {
+			w.WriteHeader(400)
+			w.Write([]byte("Chirp must be between 1 and 140 characters\n"))
+			return
+		}
+		type returnVals struct {
+			CleanedBody string `json:"cleaned_body,omitempty"`
+		}
+		data, err := json.Marshal(returnVals{CleanedBody: censorBody(params.Body)})
+		if err != nil {
+			log.Printf("Error marshalling JSON: %s", err)
+			w.WriteHeader(500)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		w.WriteHeader(200)
+		w.Write(data)
 	})
 
 	mux.HandleFunc("GET /admin/metrics", func(w http.ResponseWriter, r *http.Request) {
